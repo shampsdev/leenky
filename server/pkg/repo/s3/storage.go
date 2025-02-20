dd8f09fc9880cafa26c4491eabe9160ab3a44e1e
vaniog <vaniog@yandex-team.ru>
2025-02-21 02:36:27 +0300

    move url

--- server/pkg/repo/s3/storage.go
+++ server/pkg/repo/s3/storage.go
@@ -56,7 +56,7 @@ func (s *Storage) SavePhoto(_ context.Context, imageURL string) (string, error) 
 		fileExtension = []string{".jpeg"}
 	}
 
-	key := fmt.Sprintf("tglinked/%s%s", uniqueImageID, fileExtension[0])
+	key := fmt.Sprintf("%s%s", uniqueImageID, fileExtension[0])
 
 	_, err = s.s3Client.PutObject(&s3.PutObjectInput{
 		Bucket:      &s.cfg.Bucket,
@@ -68,7 +68,7 @@ func (s *Storage) SavePhoto(_ context.Context, imageURL string) (string, error) 
 		return "", fmt.Errorf("failed to upload image to S3: %w", err)
 	}
 
-	fileURL := fmt.Sprintf("%s/%s/%s", s.cfg.EndpointUrl, s.cfg.Bucket, url.PathEscape(key))
+	fileURL := fmt.Sprintf("%s/%s/tglinked/%s", s.cfg.EndpointUrl, s.cfg.Bucket, url.PathEscape(key))
 	log.Infof("Image uploaded successfully: %s", fileURL)
 
 	return fileURL, nil
