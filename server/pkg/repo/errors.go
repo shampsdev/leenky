3bd693d7b2ebb8ad7327c118e694c900cde37b78
vaniog <vaniog@yandex-team.ru>
2025-03-19 22:17:08 +0300

    Lint fix

--- server/pkg/repo/errors.go
+++ server/pkg/repo/errors.go
@@ -2,5 +2,7 @@ package repo
 
 import "errors"
 
-var ErrChatNotFound = errors.New("chat not found")
-var ErrUserNotFound = errors.New("user not found")
+var (
+	ErrChatNotFound = errors.New("chat not found")
+	ErrUserNotFound = errors.New("user not found")
+)
