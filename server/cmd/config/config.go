e08672efea424ce568096c10dab5cee42ef71c2d
vaniog <vaniog@yandex-team.ru>
2025-03-24 01:42:26 +0300

    json handler in prod

--- server/cmd/config/config.go
+++ server/cmd/config/config.go
@@ -100,7 +100,7 @@ func (c *Config) Logger() *slog.Logger {
 		}))
 	}
 	if c.Env == "production" {
-		return slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		return slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 			Level: slog.LevelDebug,
 		}))
 	}
