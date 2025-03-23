9efd53f1a84203ee39fb16ff3a02c908894b4f64
PriestFaria <timurchikus@gmail.com>
2025-03-23 18:26:24 +0300

    fix lint

--- client/src/pages/editProfile.page.tsx
+++ client/src/pages/editProfile.page.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import InputFieldComponent from "../components/inputField.component";
 import TextareaFieldComponent from "../components/textareaField.component";
 import { UserData, ProfileUserData } from "../types/user.interface";
