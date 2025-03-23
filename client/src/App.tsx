4a6e7223bbc4d361d95cb3855e5cda5f620e295b
PriestFaria <timurchikus@gmail.com>
2025-03-23 18:23:39 +0300

    refactor: animation duration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -18,8 +18,8 @@ import { AnimatePresence, motion } from "framer-motion";
 
 const pageVariants = {
   initial: { opacity: 0, y: 20 },
-  animate: { opacity: 1, y: 0, transition: { duration: 0.3 } },
-  exit: { opacity: 0, y: -20, transition: { duration: 0.2 } },
+  animate: { opacity: 1, y: 0, transition: { duration: 0.15 } },
+  exit: { opacity: 0, y: -20, transition: { duration: 0.15 } },
 };
 
 function App() {
