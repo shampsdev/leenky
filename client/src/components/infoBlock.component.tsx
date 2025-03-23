3ab069e39d82373ec46846847c4466fd30eb3f71
PriestFaria <timurchikus@gmail.com>
2025-03-23 21:05:54 +0300

    fix margin in profile view

--- client/src/components/infoBlock.component.tsx
+++ client/src/components/infoBlock.component.tsx
@@ -7,7 +7,7 @@ interface InfoBlockComponentProps {
 const InfoBlockComponent = (props: InfoBlockComponentProps) => {
   return (
     <div
-      className={`bg-form flex flex-col gap-[10px] px-[16px] rounded-[12px] divide-y divide-[#707579] ${props.className}`}
+      className={`bg-form flex flex-col py-[2px] px-[16px] rounded-[12px] divide-y divide-[#D9D9D9] ${props.className}`}
     >
       {props.children}
     </div>
--- client/src/components/infoParagraph.component.tsx
+++ client/src/components/infoParagraph.component.tsx
@@ -4,7 +4,7 @@ interface InfoParagraphComponentProps {
 }
 const InfoParagraphComponent = (props: InfoParagraphComponentProps) => {
   return (
-    <div className="flex flex-col py-[17px]">
+    <div className="flex flex-col py-[12px] ">
       <p className="text-[15px] text-hint">{props.title}</p>
       <p className="text-[17px] whitespace-pre-wrap break-words">
         {props.content}
